58277c5574ae8f7ef4fe15377da8098e8db99ecc
ARC4N3 <arcaneisc00l@gmail.com>
2023-03-21 07:00:13 +0530

    added comments

--- dataEntry.py
+++ dataEntry.py
@@ -1,37 +1,79 @@
 import psycopg2
+import psycopg2.extras
 
 class database():
     def __init__(self) -> None:
+        """
+        Initiate the database class by creating a cursor that connects to a (in this case) local database
+        """        
         self.cursor = self.getCursor()
         
     def getCursor(self) -> object:
+        """Creates a connection curse to perform operations in the database
+
+        Returns:
+            cursor (object): connection cursor
+        """        
 
         conn = psycopg2.connect(database="student details",user="postgres",password="12345",host="localhost",port="5432")
         conn.autocommit = True
         cur = conn.cursor()
+        self.conn = conn
 
         return cur
 
     def executeQuery(self, query, tup : tuple = None) -> None:
+        """Executes a query/Performs an operation in postgresql 
+
+        Args:
+            query (str): the query to be executed
+            tup (tuple, optional): A tuple that can be mapped to format values in the query. Defaults to None.
+        """        
+
         if tup != None:
             self.cursor.execute(query, tup)
         else:
             self.cursor.execute(query)
 
     
-    def selectUID(self, uid: str) -> None:
+    def selectUID(self, uid: str) -> tuple:
+        """Gives the row for the provided UID
+
+        Args:
+            uid (str): the user ID to identify a row
+
+        Returns:
+            tuple: resulted row that is found
+        """        
+
+        """tuple- (id, name, addr, cgpa, 10th%, 12th%, remarks, phone)"""
         query = "SELECT * FROM student_info WHERE uid = %s"
         self.executeQuery(query, tup = (uid,)) 
-        
-        return self.cursor.fetchall()[0]
 
+        res = self.cursor.fetchall()[0]
+        return res
+    
     def insertValues(self, uid: str, name: str, address: str,
                     curCGPA: float, percent10th: float, 
                     percent12th: float, remarks: str, phone: str) -> None:
+        """
+            A function to insert values in a row
+        Args:
+            uid (str): User ID (primary key)
+            name (str): Name of the user
+            address (str): Address of the user
+            curCGPA (float): current CGPA of the user
+            percent10th (float): 10th percentage of the user
+            percent12th (float): 12th percentage of the user
+            remarks (str): remarks by the mentor
+            phone (str): Phone no. of the user
+        """   
+
         query = "INSERT INTO student_info VALUES(%s, %s, %s, %s, %s, %s, %s, %s)"
         self.executeQuery(query, tup = (uid, name, address, curCGPA, percent10th, percent12th, remarks, phone))
 
 
 db = database()
-db.insertValues('6687', 'Aaryan', 'Somewhere', 8.6, 98.5, 99.9, 'Big', '9891558594')
-print(db.selectUID('6687'))
+if __name__ == "__main__":
+    db.insertValues('6687', 'Aaryan', 'Somewhere', 8.6, 98.5, 99.9, 'Big', '9891558594')
+    print(db.selectUID('6668'))
--- main.py
+++ main.py
@@ -5,6 +5,15 @@ import json, codecs
 
 class Dataset():
     def __init__(self, images : list = None, id : str = None, known_faces : list = [], known_names : list = []) -> None:
+        """Create face encodings for a user to add to the set of known users
+
+        Args:
+            images (list, optional): A list of image objects that are processed to add face encodings. Defaults to None.
+            id (str, optional): User id to which the list of images corresponds. Defaults to None.
+            known_faces (list, optional): A list of known faces. Defaults to [].
+            known_names (list, optional): A list of known names/uid that is mapped to the known faces. Defaults to [].
+        """  
+
         self.images = images
         self.id = id
         self.facesloc_ = []
@@ -14,16 +23,28 @@ class Dataset():
 
     
     def AddFaceLocations(self) -> list:
+        """Marks the locations of face features
+
+        Returns:
+            faces (list): A numpy array of face features for each image
+        """        
         faces = self.facesloc_
         for image in self.images:
             face_locations = face_recognition.face_locations(image)
             for top, right, bottom, left in face_locations:
+                
                 face_image = image[top:bottom, left:right]
                 faces.append(face_image)
         
         return faces
     
     def AddFaceEncodings(self, faces: list) -> None:
+        """Encode each face feature from the list of face features
+
+        Args:
+            faces (list): A list of face features that are marked
+        """        
+
         face_encs = []
         c = 0
         for face in faces:
@@ -36,15 +57,40 @@ class Dataset():
         self.knownNames_.extend([self.id for i in range(c)])
     
     def WriteFaces(self) -> None:
-        print(self.knownFaces_)
+        """
+        Write the json for known faces and their corresponding user ids to a file
+        """        
+        #print(self.knownFaces_)
+        print(self.knownNames_)
         toWrite = {"known faces" : list(self.knownFaces_), "known names" : list(self.knownNames_)}
-        json.dump(toWrite, open('faces.json', 'w', encoding='utf-8'), separators=(',', ':'), sort_keys=True, indent=4)
+        json.dump(toWrite, open('./flask-server/FaceCheck/faces.json', 'w', encoding='utf-8'), separators=(',', ':'), sort_keys=True, indent=4)
 
 class FacialDetection(Dataset):
+
     def __init__(self, images: list = None, id: str = None, known_faces: list = [], known_names: list = []) -> None:
+        """
+        Inherits the Dataset class and adds the 
+        functionality of identifying one particular frame/image
+
+        Args:
+            images (list, optional): A list of image objects that are processed to add face encodings. Defaults to None.
+            id (str, optional): User id to which the list of images corresponds. Defaults to None.
+            known_faces (list, optional): A list of known faces. Defaults to [].
+            known_names (list, optional): A list of known names/uid that is mapped to the known faces. Defaults to [].
+        """        
         super().__init__(images, id, known_faces, known_names)
 
     def detect(self, frame) -> str:
+        """
+        A function to identify a frame and compare the known users
+
+        Args:
+            frame (obj): frame/imaje object to be detected
+
+        Returns:
+            name (str): User ID of the identified individual. Returns Unkown if not identified
+        """    
+
         known_faces, known_names = self.knownFaces_, self.knownNames_
         face_locations = face_recognition.face_locations(frame)
         face_encodings = face_recognition.face_encodings(frame, face_locations)
@@ -60,15 +106,28 @@ class FacialDetection(Dataset):
                 match_index = matches.index(True)
                 name = known_names[match_index]
 
-            
-            return name
+                return name
+        
+        return name
 
 class ImageList():
 
     def __init__(self) -> None:
+        """
+        A class to make a dictionary of user IDs
+        with a list of their respective pictures 
+        as values
+        """        
+
         self.images = dict()
 
     def ProcessImage(self, uid, filename) -> None:
+        """Processes a file to convert it an openCV img object
+
+        Args:
+            uid (str): User ID 
+            filename (str): Path to the image that needs to be processed
+        """        
         img = cv2.imread(filename)
         if uid not in self.images.keys():
             self.images[uid] = [img]
@@ -77,13 +136,22 @@ class ImageList():
 
 
 def Register(uid: str, imagePaths: list) -> None:
+    """
+    A function to register a list of image paths that correspond to a user
+    i.e register the user to known users
+
+    Args:
+        uid (str): User id
+        imagePaths (list): A list of image paths that need to be added for processing 
+    """    
+
     imageData = ImageList()
     for path in imagePaths:
         imageData.ProcessImage(uid, path)
     
     images = imageData.images
 
-    faces = json.loads(open('faces.json').read())
+    faces = json.loads(open('./flask-server/FaceCheck/faces.json').read())
     for id in images:
         if id in faces['known names']:
             print('Id already registered')
@@ -94,31 +162,18 @@ def Register(uid: str, imagePaths: list) -> None:
         dataset.WriteFaces()
 
 def Detect(filename: str) -> str:
+    """
+    A function to detect if a particular image belongs to a known user
+
+    Args:
+        filename (str): path to the file that needs to be identified
+
+    Returns:
+        name (str): the user id that is identified. Unknown if unidentified 
+    """    
     img = cv2.imread(filename)
-    faces = json.loads(open('faces.json').read())
+    faces = json.loads(open('./flask-server/FaceCheck/faces.json').read())
     detection = FacialDetection(known_faces = faces['known faces'], known_names = faces['known names'])
     name = detection.detect(img)
     
     return name
-
-'''if __name__ == '__main__':
-    imageData = ImageList()
-    for imgFolder in os.listdir('data/'):
-        for filename in os.listdir('data/' + imgFolder):
-            filename = 'data/' + imgFolder + '/' + filename
-            imageData.ProcessImage(imgFolder, filename)
-
-    images = imageData.images
-
-    faces = json.loads(open('faces.json').read())
-    for id in images:
-        if id in faces['known names']:
-            continue
-        dataset = Dataset(images = images[id], id = id, known_faces=faces['known faces'], known_names=faces['known names'])
-        faceLocs = dataset.AddFaceLocations()
-        dataset.AddFaceEncodings(faceLocs)
-        dataset.WriteFaces()
-    
-    testImage = cv2.imread('test.jpeg')
-    detection = FacialDetection(known_faces = faces['known faces'], known_names = faces['known names'])
-    detection.detect(testImage)'''
